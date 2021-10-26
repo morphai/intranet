3c949c8690bdf34ba86b7e37475dcb139a229276
morphai <daniel.kim7x@gmail.com>
2021-10-26 19:05:10 +0900

    11 navigation drawer 만들기

--- intra/src/App.vue
+++ intra/src/App.vue
@@ -5,12 +5,20 @@
       color="primary"
       dark
     >
-      <v-app-bar-nav-icon />
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
       <v-toolbar-title>Page title</v-toolbar-title>
       <v-spacer />
       <v-btn icon to="/about"><v-icon>mdi-magazine-pistol</v-icon></v-btn>
       <v-btn icon to="/xxx"><v-icon>mdi-magazine-pistol</v-icon></v-btn>
     </v-app-bar>
+    <v-navigation-drawer app v-model="drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>Application</v-list-item-title>
+          <v-list-item-subtitle>subtitle</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-navigation-drawer>
     <v-content>
       <router-view />
     </v-content>
@@ -24,6 +32,11 @@
 <script>
 
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      drawer: false
+    }
+  }
 }
 </script>

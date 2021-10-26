36ef0857e82d45d4a006596a8d394040704d841e
morphai <daniel.kim7x@gmail.com>
2021-10-26 19:26:15 +0900

    12 component 사용하기

--- intra/src/App.vue
+++ intra/src/App.vue
@@ -6,36 +6,44 @@
       dark
     >
       <v-app-bar-nav-icon @click="drawer = !drawer" />
-      <v-toolbar-title>Page title</v-toolbar-title>
+      <site-title :title="title"></site-title>
       <v-spacer />
-      <v-btn icon to="/about"><v-icon>mdi-magazine-pistol</v-icon></v-btn>
-      <v-btn icon to="/xxx"><v-icon>mdi-magazine-pistol</v-icon></v-btn>
     </v-app-bar>
     <v-navigation-drawer app v-model="drawer">
       <v-list-item>
         <v-list-item-content>
-          <v-list-item-title>Application</v-list-item-title>
-          <v-list-item-subtitle>subtitle</v-list-item-subtitle>
+          <v-list-item-title>
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtitle
+          </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
+      <v-divider></v-divider>
+      <site-menu></site-menu>
     </v-navigation-drawer>
     <v-content>
       <router-view />
     </v-content>
-    <v-footer app color="primary" dark absolute>
-      <v-spacer></v-spacer>
-      <div>SHINWHAINTECH</div>
-    </v-footer>
+    <site-footer :footer="footer"></site-footer>
   </v-app>
 </template>
 
 <script>
 
+import siteTitle from '@/views/site/title'
+import siteFooter from '@/views/site/footer'
+import siteMenu from '@/views/site/menu'
 export default {
+  components: { siteTitle, siteFooter, siteMenu },
   name: 'App',
   data () {
     return {
-      drawer: false
+      drawer: false,
+      item: [],
+      title: 'title 입니다.',
+      footer: 'footer 입니다.'
     }
   }
 }

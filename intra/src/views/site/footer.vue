f19eec991721934ef68963e9d47254c92e1de82a
morphai <daniel.kim7x@gmail.com>
2021-10-27 12:46:54 +0900

    19 에러처리(toast 문제점발견 후 조치완료)

--- intra/src/views/site/footer.vue
+++ intra/src/views/site/footer.vue
@@ -1,18 +1,18 @@
 <template>
-  <v-footer app color="primary" dark absolute>
-    <v-spacer />
-    {{ footer }}
+  <v-footer app color="primary" dark absolute :footer="footer">
+    <v-spacer></v-spacer>
+    <div>&copy; {{ new Date().getFullYear() + ' ' + footer }}</div>
     <v-btn icon @click="openDialog"><v-icon>mdi-pencil</v-icon></v-btn>
     <v-dialog v-model="dialog" max-width="400">
       <v-card>
         <v-card-title>
-          제목수정
-          <v-spacer />
+          제목 수정
+          <v-spacer/>
           <v-btn icon @click="save"><v-icon>mdi-content-save</v-icon></v-btn>
           <v-btn icon @click="dialog=false"><v-icon>mdi-close</v-icon></v-btn>
         </v-card-title>
         <v-card-text>
-          <v-text-field v-model="text" outlined label="제목" @keypress.enter="save" hide-detail></v-text-field>
+          <v-text-field v-model="text" outlined label="제목" @keypress.enter="save" hide-details />
         </v-card-text>
       </v-card>
     </v-dialog>
@@ -34,8 +34,6 @@ export default {
     async save () {
       try {
         await this.$firebase.database().ref().child('site').update({ footer: this.text })
-      } catch (e) {
-        console.log(e.message)
       } finally {
         this.dialog = false
       }
--- intra/src/views/site/title.vue
+++ intra/src/views/site/title.vue
@@ -5,13 +5,13 @@
     <v-dialog v-model="dialog" max-width="400">
       <v-card>
         <v-card-title>
-          제목수정
-          <v-spacer />
+          제목 수정
+          <v-spacer/>
           <v-btn icon @click="save"><v-icon>mdi-content-save</v-icon></v-btn>
           <v-btn icon @click="dialog=false"><v-icon>mdi-close</v-icon></v-btn>
         </v-card-title>
         <v-card-text>
-          <v-text-field v-model="text" outlined label="제목" @keypress.enter="save" hide-detail></v-text-field>
+          <v-text-field v-model="text" outlined label="제목" @keypress.enter="save" hide-details />
         </v-card-text>
       </v-card>
     </v-dialog>
@@ -33,8 +33,6 @@ export default {
     async save () {
       try {
         await this.$firebase.database().ref().child('site').update({ title: this.text })
-      } catch (e) {
-        console.log(e.message)
       } finally {
         this.dialog = false
       }

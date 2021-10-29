d04186daad17b79c100eee480401a0daa0d42eec
morphai <daniel.kim7x@gmail.com>
2021-10-29 14:12:47 +0900

    44 사용자 정보 표시하기

---
+++ intra/src/components/display-user.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-tooltip bottom>
+    <template v-slot:activator="{on}">
+      <v-chip v-on="on" color="primary">
+        <v-avatar left>
+          <v-img :src="user.photoURL"></v-img>
+        </v-avatar>
+        {{user.displayName}}
+      </v-chip>
+    </template>
+    <span>{{user.email}}</span>
+  </v-tooltip>
+</template>
+<script>
+export default {
+  props: ['user']
+}
+</script>

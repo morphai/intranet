f42c7f442a454bd824fa31e00f651f9b2d66754b
morphai <daniel.kim7x@gmail.com>
2021-10-29 09:58:52 +0900

    41 파이어스토어로 사용자 다루기

--- intra/functions/index.js
+++ intra/functions/index.js
@@ -4,7 +4,7 @@ var serviceAccount = require('./key.json')
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: functions.config().admin.db_url // 'https://shinwhaintranet-default-rtdb.firebaseio.com'
+  databaseURL: functions.config().admin.db_url // 'https://memi-vf2.firebaseio.com'
 })
 
 const db = admin.database()
@@ -12,19 +12,23 @@ const fdb = admin.firestore()
 
 exports.createUser = functions.auth.user().onCreate(async (user) => {
   const { uid, email, displayName, photoURL } = user
+  const time = new Date()
   const u = {
     email,
     displayName,
     photoURL,
-    createdAt: new Date().getTime(),
+    createdAt: time,
     level: email === functions.config().admin.email ? 0 : 5
   }
-  db.ref('users').child(uid).set(u)
+  await fdb.collection('users').doc(uid).set(u)
+  u.createdAt = time.getTime()
+  await db.ref('users').child(uid).set(u)
 })
 
 exports.deleteUser = functions.auth.user().onDelete(async (user) => {
   const { uid } = user
-  db.ref('users').child(uid).remove()
+  await db.ref('users').child(uid).remove()
+  await fdb.collection('users').doc(uid).delete()
 })
 
 exports.incrementBoardCount = functions.firestore.document('boards/{bid}').onCreate(async (snap, context) => {
@@ -33,8 +37,8 @@ exports.incrementBoardCount = functions.firestore.document('boards/{bid}').onCre
   } catch (e) {
     await fdb.collection('meta').doc('boards').set({ count: 1 })
   }
-  })
+})
 
-  exports.decrementBoardCount = functions.firestore.document('boards/{bid}').onDelete(async (snap, context) => {
-    await fdb.collection('meta').doc('boards').update('count', admin.firestore.FieldValue.increment(-1))
-  })
+exports.decrementBoardCount = functions.firestore.document('boards/{bid}').onDelete(async (snap, context) => {
+  await fdb.collection('meta').doc('boards').update('count', admin.firestore.FieldValue.increment(-1))
+})

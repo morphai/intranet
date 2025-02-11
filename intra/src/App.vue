3b7392567dafbd214c451f3de58bd0b48a6996af
morphai <daniel.kim7x@gmail.com>
2025-02-11 10:35:21 +0900

    A62. 인원추가(품질/정진우)

--- intra/src/App.vue
+++ intra/src/App.vue
@@ -1,27 +1,57 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      dense
-    >
+    <v-app-bar app dense>
       <v-app-bar-nav-icon @click="drawer = !drawer" />
-      <v-btn class="ma-2" elevation="2" :href="'https://login.ecount.com/Login/KR/'" target="_blank">
+      <v-btn
+        class="ma-2"
+        elevation="2"
+        :href="'https://login.ecount.com/Login/KR/'"
+        target="_blank"
+      >
         ERP
       </v-btn>
       <v-spacer />
       <!-- <site-title :title="site.title"></site-title> -->
       <v-spacer />
       <template v-if="user">
-        <v-icon left>mdi-card-account-details</v-icon>{{user.displayName}}
+        <v-icon left>mdi-card-account-details</v-icon>{{ user.displayName }}
         <v-icon left class="ml-5">mdi-email</v-icon>
-        <span v-if="user.email == 'justice0408@gmail.com'">khkim@shinwhaintec.co.kr</span> <!--관리자-->
-        <span v-else-if="user.email == 'daniel.kim7x@gmail.com'">khkim@shinwhaintec.co.kr</span> <!--developer-->
-        <span v-else-if="user.email == 'kslee2471@gmail.com'">kslee@shinwhaintec.co.kr</span> <!--품질/이기섭-->
-        <span v-else-if="user.email == 'jgh208011@gmail.com'">ghjung@shinwhaintec.co.kr</span> <!--생산/정귀홍-->
-        <span v-else-if="user.email == 'k01084119633@gmail.com'">k01084119633@gmail.com</span> <!--품질/곽명순-->
-        <span v-else-if="user.email == 'kohh8611@gmail.com'">hhko@shinwhaintec.co.kr</span> <!--영업/고현호-->
-        <span v-else-if="user.email == 'jaewan77777@gmail.com'">jwpark@shinwhaintec.co.kr</span> <!--구매/박재완-->
-        <span v-else-if="user.email == 'solitury12@gmail.com'">bjkim@shinwhaintec.co.kr</span> <!--금형/개발팀/김범준-->
+        <span v-if="user.email == 'justice0408@gmail.com'"
+          >khkim@shinwhaintec.co.kr</span
+        >
+        <!--관리자-->
+        <span v-else-if="user.email == 'daniel.kim7x@gmail.com'"
+          >khkim@shinwhaintec.co.kr</span
+        >
+        <!--developer-->
+        <span v-else-if="user.email == 'kslee2471@gmail.com'"
+          >kslee@shinwhaintec.co.kr</span
+        >
+        <!--품질/이기섭-->
+        <span v-else-if="user.email == 'jgh208011@gmail.com'"
+          >ghjung@shinwhaintec.co.kr</span
+        >
+        <!--생산/정귀홍-->
+        <span v-else-if="user.email == 'k01084119633@gmail.com'"
+          >k01084119633@gmail.com</span
+        >
+        <!--품질/곽명순-->
+        <span v-else-if="user.email == 'kohh8611@gmail.com'"
+          >hhko@shinwhaintec.co.kr</span
+        >
+        <!--영업/고현호-->
+        <span v-else-if="user.email == 'jaewan77777@gmail.com'"
+          >jwpark@shinwhaintec.co.kr</span
+        >
+        <!--구매/박재완-->
+        <span v-else-if="user.email == 'solitury12@gmail.com'"
+          >bjkim@shinwhaintec.co.kr</span
+        >
+        <!--금형/개발팀/김범준-->
+        <span v-else-if="user.email == 'qpaldm1@gmail.com'"
+          >jwjung@shinwhaintec.co.kr</span
+        >
+        <!--품질팀/정진우-->
         <!--
           <span v-else-if="user.email == 'yain369@gmail.com'">sjpark@shinwhaintec.co.kr</span>
         -->
@@ -46,71 +76,78 @@
 </template>
 
 <script>
-
 // import siteTitle from '@/views/site/title'
 // import siteFooter from '@/views/site/footer'
-import siteMenu from '@/views/site/menu'
-import siteSign from '@/views/site/sign'
+import siteMenu from "@/views/site/menu";
+import siteSign from "@/views/site/sign";
 export default {
   components: { siteMenu, siteSign }, // siteTitle, siteFooter 제외
-  name: 'App',
-  data () {
+  name: "App",
+  data() {
     return {
       drawer: false,
       site: {
         menu: [
           {
-            title: 'home',
-            icon: 'mdi-home',
+            title: "home",
+            icon: "mdi-home",
             subItems: [
               {
-                title: 'Dashboard',
-                to: '/'
+                title: "Dashboard",
+                to: "/",
               },
               {
-                title: 'About',
-                to: '/about'
-              }
-            ]
+                title: "About",
+                to: "/about",
+              },
+            ],
           },
           {
-            title: 'about',
-            icon: 'mdi-information',
+            title: "about",
+            icon: "mdi-information",
             active: true,
             subItems: [
               {
-                title: 'xxx',
-                to: '/xxx'
-              }
-            ]
-          }
+                title: "xxx",
+                to: "/xxx",
+              },
+            ],
+          },
         ],
-        title: 'Intranet',
-        footer: 'Shinwhaintech Co.,Ltd'
-      }
-    }
+        title: "Intranet",
+        footer: "Shinwhaintech Co.,Ltd",
+      },
+    };
   },
-  created () {
-    this.subscribe()
+  created() {
+    this.subscribe();
   },
   computed: {
-    user () {
-      return this.$store.state.user
-    }
+    user() {
+      return this.$store.state.user;
+    },
   },
   methods: {
-    subscribe () {
-      this.$firebase.database().ref().child('site').on('value', (sn) => {
-        const v = sn.val()
-        if (!v) {
-          this.$firebase.database().ref().child('site').set(this.site)
-          return
-        }
-        this.site = v
-      }, (e) => {
-        console.log(e.message)
-      })
-    }
-  }
-}
+    subscribe() {
+      this.$firebase
+        .database()
+        .ref()
+        .child("site")
+        .on(
+          "value",
+          (sn) => {
+            const v = sn.val();
+            if (!v) {
+              this.$firebase.database().ref().child("site").set(this.site);
+              return;
+            }
+            this.site = v;
+          },
+          (e) => {
+            console.log(e.message);
+          }
+        );
+    },
+  },
+};
 </script>
--- intra/src/components/display-user.vue
+++ intra/src/components/display-user.vue
@@ -1,31 +1,69 @@
 <template>
   <v-tooltip bottom>
-    <template v-slot:activator="{on}">
+    <template v-slot:activator="{ on }">
       <v-chip v-on="on">
         <v-avatar left>
           <v-img :src="user.photoURL"></v-img>
         </v-avatar>
-        <span v-if="user.email == 'justice0408@gmail.com'">대표이사 / 김기현</span>
-        <span v-else-if="user.email == 'daniel.kim7x@gmail.com'">개발자 / daniel</span>
-        <span v-else-if="user.email == 'kslee2471@gmail.com'">품질팀 / 이기섭</span>
-        <span v-else-if="user.email == 'jgh208011@gmail.com'">생산팀 / 정귀홍</span>
-        <span v-else-if="user.email == 'k01084119633@gmail.com'">품질팀 / 곽명순</span>
-        <span v-else-if="user.email == 'kohh8611@gmail.com'">영업팀 / 고현호</span>
-        <span v-else-if="user.email == 'jaewan77777@gmail.com'">구매팀 / 박재완</span>
-        <span v-else-if="user.email == 'solitury12@gmail.com'">금형/개발팀 / 김범준</span>
+        <span v-if="user.email == 'justice0408@gmail.com'"
+          >대표이사 / 김기현</span
+        >
+        <span v-else-if="user.email == 'daniel.kim7x@gmail.com'"
+          >개발자 / daniel</span
+        >
+        <span v-else-if="user.email == 'kslee2471@gmail.com'"
+          >품질팀 / 이기섭</span
+        >
+        <span v-else-if="user.email == 'jgh208011@gmail.com'"
+          >생산팀 / 정귀홍</span
+        >
+        <span v-else-if="user.email == 'k01084119633@gmail.com'"
+          >품질팀 / 곽명순</span
+        >
+        <span v-else-if="user.email == 'kohh8611@gmail.com'"
+          >영업팀 / 고현호</span
+        >
+        <span v-else-if="user.email == 'jaewan77777@gmail.com'"
+          >구매팀 / 박재완</span
+        >
+        <span v-else-if="user.email == 'solitury12@gmail.com'"
+          >금형/개발팀 / 김범준</span
+        >
+        <span v-else-if="user.email == 'qpaldm1@gmail.com'"
+          >품질팀 / 정진우</span
+        >
         <!--
           <span v-else-if="user.email == 'yain369@gmail.com'">개발팀 / 박석진</span>
         -->
       </v-chip>
     </template>
-    <span v-if="user.email == 'justice0408@gmail.com'">khkim@shinwhaintec.co.kr | 010-7105-4882</span>
-    <span v-else-if="user.email == 'daniel.kim7x@gmail.com'">khkim@shinwhaintec.co.kr | 010-7105-4882</span>
-    <span v-else-if="user.email == 'kslee2471@gmail.com'">kslee@shinwhaintec.co.kr | 010-4022-2471</span>
-    <span v-else-if="user.email == 'jgh208011@gmail.com'">ghjung@shinwhaintec.co.kr | 010-3821-3299</span>
-    <span v-else-if="user.email == 'k01084119633@gmail.com'">msgwak@shinwhaintec.co.kr | 010-8411-9633</span>
-    <span v-else-if="user.email == 'kohh8611@gmail.com'">hhko@shinwhaintec.co.kr | 010-4641-8611</span>
-    <span v-else-if="user.email == 'jaewan77777@gmail.com'">jwpark@shinwhaintec.co.kr | 010-2706-2439</span>
-    <span v-else-if="user.email == 'solitury12@gmail.com'">bjkim@shinwhaintec.co.kr | 010-4366-1929</span>
+    <span v-if="user.email == 'justice0408@gmail.com'"
+      >khkim@shinwhaintec.co.kr | 010-7105-4882</span
+    >
+    <span v-else-if="user.email == 'daniel.kim7x@gmail.com'"
+      >khkim@shinwhaintec.co.kr | 010-7105-4882</span
+    >
+    <span v-else-if="user.email == 'kslee2471@gmail.com'"
+      >kslee@shinwhaintec.co.kr | 010-4022-2471</span
+    >
+    <span v-else-if="user.email == 'jgh208011@gmail.com'"
+      >ghjung@shinwhaintec.co.kr | 010-3821-3299</span
+    >
+    <span v-else-if="user.email == 'k01084119633@gmail.com'"
+      >msgwak@shinwhaintec.co.kr | 010-8411-9633</span
+    >
+    <span v-else-if="user.email == 'kohh8611@gmail.com'"
+      >hhko@shinwhaintec.co.kr | 010-4641-8611</span
+    >
+    <span v-else-if="user.email == 'jaewan77777@gmail.com'"
+      >jwpark@shinwhaintec.co.kr | 010-2706-2439</span
+    >
+    <span v-else-if="user.email == 'solitury12@gmail.com'"
+      >bjkim@shinwhaintec.co.kr | 010-4366-1929</span
+    >
+    <span v-else-if="user.email == 'qpaldm1@gmail.com'"
+      >jwjung@shinwhaintec.co.kr | 010-4142-1477</span
+    >
     <!--
       <span v-else-if="user.email == 'yain369@gmail.com'">sjpark@shinwhaintec.co.kr | 010-2292-3888</span>
     -->
@@ -33,8 +71,8 @@
 </template>
 <script>
 export default {
-  props: ['user']
-}
+  props: ["user"],
+};
 </script>
 
 <!--
